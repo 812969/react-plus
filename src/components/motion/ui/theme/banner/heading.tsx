b756cfd996f23ed992a208ab03ff4bf780a2d796
zlm <15290464265@163.com>
2025-06-19 22:57:36 +0800

    feat: banner使用motion实现动画

--- src/components/motion/ui/theme/banner/heading.tsx
+++ src/components/motion/ui/theme/banner/heading.tsx
@@ -1,16 +1,38 @@
+'use client';
 
+import React, {useEffect, useRef} from "react";
+import {splitText} from "motion-plus";
+import {animate, stagger} from "motion";
 export function Heading(){
+
+  const ref = useRef(false);
+  useEffect((()=>{
+    document.fonts.ready.then(()=>{
+      if(ref.current){
+        const element = ref.current as unknown as HTMLDivElement;
+        element.style.visibility = 'visible';
+        const {chars} = splitText(element);
+        animate(chars,
+          {
+            opacity: [0,1],
+            y: [10,0]
+          },
+          {
+            type:'spring',
+            duration:2,
+            delay: stagger(0.1)
+          }
+        )
+
+      }
+    })
+  }))
+
   return (
     <section className={'bg-slate-200 py-32'}>
       <div className={'max-w-7xl mx-auto h-2 flex flex-col justify-center items-center gap-y-5'}>
-        <span className={'text-5xl md:text-7xl lg:text-9xl'}>
-          <span className={'animate-move animate-delay-1000'}>E</span>
-          <span className={'animate-move animate-delay-1500'}>x</span>
-          <span className={'animate-move animate-delay-2000'}>a</span>
-          <span className={'animate-move animate-delay-2500'}>m</span>
-          <span className={'animate-move animate-delay-3000'}>p</span>
-          <span className={'animate-move animate-delay-3500'}>l</span>
-          <span className={'animate-move animate-delay-4000'}>e</span>
+        <span ref={ref} className={'text-5xl md:text-7xl lg:text-9xl invisible'}>
+          Example
         </span>
         <h2 className={'text-center text-base md:text-2xl lg:text-3xl'}>
           Each example includes ui & video explanation & pretty source code
