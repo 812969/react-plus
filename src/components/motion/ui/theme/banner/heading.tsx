0cd598079590ad05fe27ff1428364b2e658ca9d0
zlm <15290464265@163.com>
2025-06-19 22:27:36 +0800

    feat: banner使用css实现动画

--- src/components/motion/ui/theme/banner/heading.tsx
+++ src/components/motion/ui/theme/banner/heading.tsx
@@ -4,13 +4,19 @@ export function Heading(){
     <section className={'bg-slate-200 py-32'}>
       <div className={'max-w-7xl mx-auto h-2 flex flex-col justify-center items-center gap-y-5'}>
         <span className={'text-5xl md:text-7xl lg:text-9xl'}>
-          Example
+          <span className={'animate-move animate-delay-1000'}>E</span>
+          <span className={'animate-move animate-delay-1500'}>x</span>
+          <span className={'animate-move animate-delay-2000'}>a</span>
+          <span className={'animate-move animate-delay-2500'}>m</span>
+          <span className={'animate-move animate-delay-3000'}>p</span>
+          <span className={'animate-move animate-delay-3500'}>l</span>
+          <span className={'animate-move animate-delay-4000'}>e</span>
         </span>
         <h2 className={'text-center text-base md:text-2xl lg:text-3xl'}>
           Each example includes ui & video explanation & pretty source code
         </h2>
         <h3 className={'text-4xl md:text-5xl lg:text-6xl'}>
-          ctrl+f
+          ctrl + f
         </h3>
       </div>
     </section>
