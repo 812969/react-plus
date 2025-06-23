453f609500aad5e88b97da193685da6418178821
zlm <15290464265@163.com>
2025-06-23 21:16:00 +0800

    feat: counter动画

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import * as motion from 'motion/react-client'
 import {cn} from "@/lib/tailwind";
 import Nav from '@/components/motion/ui/nav/nav'
 import {Heading} from "@/components/motion/ui/theme/banner/heading";
+import { CounterContainer } from '@/components/motion/ui/counter/counter-container';
 export default function Page(){
   return (
     <div>
       <Nav></Nav>
       <Heading/>
+      <CounterContainer/>
     </div>
   )
 }
